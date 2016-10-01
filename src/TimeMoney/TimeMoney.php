17c4d2d2c489e14cdd2994e510983040f2e4b62f
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-10-01 06:59:57 -0700

    Update TimeMoney.php

--- src/TimeMoney/TimeMoney.php
+++ src/TimeMoney/TimeMoney.php
@@ -2,9 +2,10 @@
 namespace TimeMoney;
 class TimeMoney extends \pocketmine\plugin\PluginBase{
     public $array = array();
+    public $config;
     public function onEnable(){
         @mkdir($this->getDataFolder());
-        $cfg = ["Money" => 1,"Time" => 5];
+        $cfg = ["Money" => 1,"Time" => 5,];
         $this->getServer()->getPluginManager()->registerEvents(new Event(), $this);
         $this->config = new \pocketmine\utils\Config($this->getDataFolder()."config.yml",\pocketmine\utils\Config::YAML,$cfg);
         $this->config->save();

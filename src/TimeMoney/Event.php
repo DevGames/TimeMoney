78660c369d829b350eb749bc9b1bc34f25c455c4
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-10-01 06:41:53 -0700

    Update Event.php

--- src/TimeMoney/Event.php
+++ src/TimeMoney/Event.php
@@ -2,10 +2,10 @@
 namespace TimeMoney;
 class Event extends ArrayT implements \pocketmine\event\Listener{
     public function onJoin(\pocketmine\event\player\PlayerJoinEvent $ev){
-        $this->getVar()->array[$ev->getPlayer()->getName()] = time() - $this->getVar()->config->get("Time");   
+        $this->getVar()->array[$ev->getPlayer()->getName()] = time(); 
     }
     public function Packet(\pocketmine\event\server\DataPacketReceiveEvent $ev){
-        if($this->getTime($this->getVar()->array, $ev->getPlayer()->getName()) >= time()){
+        if($this->getTime($this->getVar()->array, $ev->getPlayer()->getName()) >= time() - $this->getVar()->config->get("Time")){
             $this->getVar()->array[$ev->getPlayer()->getName()] = time();
             \onebone\economyapi\EconomyAPI::getInstance()->addMoney($ev->getPlayer()->getName(),$this->getVar()->config->get("Money"));
         }

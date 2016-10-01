648069d76a96d0bd503fa5714b7bd0efcc83cfd2
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-10-01 05:24:37 -0700

    Create Base.php

---
+++ src/TimeMoney/Base.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace TimeMoney;
+
+class Base extends TimeMoney {
+        
+    public function getVar(){
+        return $this;
+    }
+    
+    public function getFunction(){
+        return $this;
+    }
+    
+}
+?>

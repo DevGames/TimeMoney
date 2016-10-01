23a69002481113a1e59b3c4dee26e9bba01b8c7f
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-10-01 09:02:41 -0700

    Update Event.php

--- src/TimeMoney/Event.php
+++ src/TimeMoney/Event.php
@@ -1,9 +1,13 @@
 <?php
+
 namespace TimeMoney;
+
 class Event extends ArrayT implements \pocketmine\event\Listener{
+    
     public function onJoin(\pocketmine\event\player\PlayerJoinEvent $ev){
         $this->getVar()->array[$ev->getPlayer()->getName()] = time(); 
     }
+    
     public function Packet(\pocketmine\event\server\DataPacketReceiveEvent $ev){
         if($this->getTime($this->getVar()->array, $ev->getPlayer()->getName()) >= time() - $this->getVar()->config->get("Time")){
             $this->getVar()->array[$ev->getPlayer()->getName()] = time();

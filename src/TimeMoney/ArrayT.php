958650236636ac38224ec4671cf1ed3d6784e710
Khaled_Craft1b <iiBa4x@gmail.com>
2016-10-01 16:21:52 +0300

    Update ArrayT.php

--- src/TimeMoney/ArrayT.php
+++ src/TimeMoney/ArrayT.php
@@ -1,15 +1,11 @@
 <?php
-
 namespace TimeMoney;
-
 class ArrayT extends Base{
-    
-     public function getTime($array,$index){
-      if(!is_array($array) || $index == $this){
-          return;
-       }
-       return $array[$index];
-     }
-
+    public function getTime($array,$index){
+        if(!is_array($array) || $index == $this){
+            return;
+        }
+        return $array[$index];
+    }
 }
 ?>

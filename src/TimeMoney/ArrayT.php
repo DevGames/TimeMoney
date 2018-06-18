01b48e878341034d26c2d2e253691e4ab0cc3049
Nawaf_Craft1b <Nawafmg97@gmail.com>
2018-06-18 12:55:38 +0300

    Update ArrayT.php

--- src/TimeMoney/ArrayT.php
+++ src/TimeMoney/ArrayT.php
@@ -2,7 +2,7 @@
 namespace TimeMoney;
 class ArrayT extends Base{
     public function getTime($array,$index){ # not used Now :)
-        if(!is_array($array) || $index == false){
+        if(!is_array($array) || $index == null){
             return;
         }
         return $array[$index];
